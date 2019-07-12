ed056990e157604fd1c9a0d4312bf3f3fc02a81f
XWQ2018 <18476969365@163.com>
2019-07-12 09:22:31 +0800

    增加过滤器函数    回滚事务开启

--- myblog/app/Http/Controllers/createUserController.php
+++ myblog/app/Http/Controllers/createUserController.php
@@ -43,6 +43,24 @@ class createUserController extends Controller
         'update_time' => $currentTime,
         'nickname' => $nickName,
         ]); */
+        /* 
+        如需过滤变量，请使用下面的过滤器函数之一：
+        filter_var() - 通过一个指定的过滤器来过滤单一的变量
+        filter_var_array() - 通过相同的或不同的过滤器来过滤多个变量
+        filter_input - 获取一个输入变量，并对它进行过滤
+        filter_input_array - 获取多个输入变量，并通过相同的或不同的过滤器对它们进行过
+        */
+        $int = '123abv';
+
+        if (!filter_var($int, FILTER_VALIDATE_INT)) {
+            echo ("Integer is not valid");
+        } else {
+            echo ("Integer is valid");
+        }
+
+
+
+        DB::beginTransaction();  //开启事务
         try {
             $ret = DB::TABLE('user')->insertGetId([
                 'username' => $userName,
@@ -56,16 +74,19 @@ class createUserController extends Controller
             $dateTime = date("Y-m-d H:i:s", time());
             print_r($ret);
             echo '<br/><hr/>';
+            DB::commit();
             return [
                 'code' => '20000',
                 'msg' => '数据插入成功',
                 'time' => $dateTime,
             ];
-
         } catch (PDOException $e) {
+
+            DB::rollBack(); //回滚操作
 
             print_r($e);
         }
+
 
         // print_r($userName . '=>' . $userPassword);
     }
